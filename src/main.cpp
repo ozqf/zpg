7690f0d6ab65fc82f09017388c6a9d410f036e2b
zqf <zqf@zealousquakefan.com>
2020-01-18 01:04:50 +0000

    WIP adding command line parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,88 @@
 
 #include <stdio.h>
+#include <string.h>
 
 #include "zpg.h"
 
+static void print_help(char* exeName)
+{
+	printf("--- Help ---\n");
+	printf("\nRun a generator preset:\n");
+	printf("preset <preset_mode> <output_file_name>\n");
+	printf("\tPreset modes are currently 1-12\n");
+	printf("\nExecute a build script:\n");
+	printf("script <script_file_name> <output_file_name>\n");
+	printf("\teg\n%s script make_dungeon.txt dungeon.txt\n", exeName);
+}
+
+static void run_preset(char* modeStr, char* outputFileName)
+{
+	i32 mode = atoi(modeStr);
+	//ZPG_RunPreset(1); // standard drunken walk
+	//ZPG_RunPreset(2); // scattered walks
+	//ZPG_RunPreset(3); // cave gen
+	//ZPG_RunPreset(4); // walks from line segment
+	//ZPG_RunPreset(5); // test draw lines
+	//ZPG_RunPreset(6); // walk with space
+	ZPG_RunPreset(7); // perlin caves
+	//ZPG_RunPreset(8);
+	//ZPG_RunPreset(9); // embed
+	//ZPG_RunPreset(10); // blit
+	//ZPG_RunPreset(11); // walk fnrom prefab
+	ZPG_RunPreset(12); // walk between prefabs via line segment
+	#if 0
+	//printf("Press ENTER to exit\n");
+	//getchar();
+	#endif
+}
+
+static void run_script(char* inputFileName, char* outputFileName)
+{
+	i32 dashes = 80;
+	while (dashes-- > 0) { printf("-"); }
+	printf("\n");
+	char* path = inputFileName;
+	FILE* f;
+	fopen_s(&f, path, "r");
+	if (f == NULL)
+	{
+		printf("Failed to open script %s\n", path);
+		return;
+	}
+	fseek(f, 0, SEEK_END);
+	i32 size = ftell(f);
+	printf("Reading %d chars\n", size);
+	fseek(f, 0, SEEK_SET);
+	u8* buf = (u8*)malloc(size);
+	fread(buf, 1, size, f);
+	fclose(f);
+	printf("Read script:\n%s\n", buf);
+	ZPG_RunScript(buf, size);
+	free(buf);
+
+}
+
 int main(int argc, char** argv)
 {
-	printf("Zealous Procedural Generator Init\n");
+	printf("Zealous Procedural Generator build %s, %s\n",
+		__DATE__, __TIME__);
+	if (argc != 4)
+	{
+		printf("No command specified\n");
+		print_help(argv[0]);
+		return 0;
+	}
 	ZPG_Init();
+	if (strcmp(argv[1], "preset") == 0)
+	{
+		run_preset(argv[2], argv[3]);
+		return 0;
+	}
+	if (strcmp(argv[1], "script") == 0)
+	{
+		run_script(argv[2], argv[3]);
+		return 0;
+	}
 	#if 0
 	printf("Read %d params\n", argc);
 	if (argc == 1)
@@ -31,28 +107,7 @@ int main(int argc, char** argv)
 	}
 	#endif
 	#if 1
-	i32 dashes = 80;
-	while (dashes-- > 0) { printf("-"); }
-	printf("\n");
-	char* path = "test_script.txt";
-	FILE* f;
-	fopen_s(&f, path, "r");
-	if (f == NULL)
-	{
-		printf("Failed to open script %s\n", path);
-		return 0;
-	}
-	fseek(f, 0, SEEK_END);
-	i32 size = ftell(f);
-	printf("Reading %d chars\n", size);
-	fseek(f, 0, SEEK_SET);
-	u8* buf = (u8*)malloc(size);
-	fread(buf, 1, size, f);
-	fclose(f);
-	printf("Read script:\n%s\n", buf);
-	ZPG_RunScript(buf, size);
-	free(buf);
-
+	
 	#endif
 
 	printf("...Done\n");
--- src/zpg/zpg_script.h
+++ src/zpg/zpg_script.h
@@ -8,18 +8,19 @@ static void ZPG_ExecuteLine(u8* cursor, u8* end)
     i32 lineLength = end - cursor;
     printf("Exec line (%d chars)\n", lineLength + 1);
     u8* lineBuf = (u8*)malloc(lineLength + 1);
-    memset(lineBuf, 0, lineLength);
+    memset(lineBuf, 0, lineLength + 1);
     memcpy(lineBuf, cursor, lineLength);
     printf("Read line \"%s\"\n", lineBuf);
     free(lineBuf);
 }
 
-static u8* ZPG_ScanForLineEnd(u8* buf, u8* end)
+static u8* ZPG_ScanForLineEnd(u8* buf, u8* end, i32* lineEndSize)
 {
     u8* result = buf;
     while (result < end)
     {
-        if (*result == '\n') { break; }
+        if (*result == '\n') { *lineEndSize = 1; break; }
+        else if (*result == '\r') { *lineEndSize = 2; break; }
         result++;
     }
     return result;
@@ -33,7 +34,8 @@ ZPG_EXPORT i32 ZPG_RunScript(u8* text, i32 textLength)
     u8* cursorEnd;
     while (cursor < end)
     {
-        cursorEnd = ZPG_ScanForLineEnd(cursor, end);
+        i32 lineEndSize;
+        cursorEnd = ZPG_ScanForLineEnd(cursor, end, &lineEndSize);
         i32 len = cursorEnd - cursor;
         if (len >= 2)
         {
@@ -43,7 +45,7 @@ ZPG_EXPORT i32 ZPG_RunScript(u8* text, i32 textLength)
         {
             printf("Skip line\n");
         }
-        cursor = cursorEnd + 1;
+        cursor = cursorEnd + lineEndSize;
     }
     return 0;
 }
